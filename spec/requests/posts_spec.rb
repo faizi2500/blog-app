e1b78af0632301f00339aa1324b767356e0e5ad7
faizi2500 <faizan.zahid2500@gmail.com>
2021-12-02 15:05:27 +0500

    Add tests to check no other template is rendered

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -11,8 +11,12 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to render_template('index')
     end
 
+    it 'renders correct template' do
+      expect(response).to_not render_template('show')
+    end
+
     it 'check for placeholder text' do
-      expect(response.body).to include 'Posts of user'
+      expect(response.body).to include('Posts of user')
     end
   end
 
@@ -26,8 +30,12 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to render_template('show')
     end
 
+    it 'renders correct template' do
+      expect(response).to_not render_template('index')
+    end
+
     it 'check for placeholder text' do
-      expect(response.body).to include 'Posts 4 by user'
+      expect(response.body).to include('Posts 4 by user')
     end
   end
 end
