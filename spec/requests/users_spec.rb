dc85bba677c37507b52c15b4a122293b41bd0a23
faizi2500 <faizan.zahid2500@gmail.com>
2021-12-02 14:32:27 +0500

    Add tests for index action and show action in users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    # pending "add some examples (or delete) #{__FILE__}"
     before(:example) { get(users_path) }
     it 'check status' do
       expect(response).to have_http_status(:ok)
@@ -12,12 +11,33 @@ RSpec.describe 'Users', type: :request do
       expect(response).to render_template('index')
     end
 
-    it 'renders correct heading' do
+    it 'check for placeholder text' do
       expect(response.body).to include 'All Users'
     end
 
-    it 'renders correct heading' do
+    it 'check for placeholder text' do
       expect(response.body).to include 'Here is a list of all users'
     end
+  end
+
+  describe 'GET /show' do
+    before(:example) { get('/users/745') }
+    
+    it 'check status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders correct template' do
+      expect(response.body).to render_template('show')
+    end
+
+    it 'check for placeholder text' do
+      expect(response.body).to include 'Users ID 745'
+    end
+
+    it 'check for placeholder text' do
+      expect(response.body).to include 'This user details'
+    end
+
   end
 end
