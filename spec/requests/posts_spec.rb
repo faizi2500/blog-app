d7889bb5a1e22f12596c1b41f6732f6bbfcdaa38
faizi2500 <faizan.zahid2500@gmail.com>
2021-12-02 14:37:30 +0500

    Add tests for index and show actions of posts controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,33 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "Posts", type: :request do
-#   describe "GET /index" do
-#     pending "add some examples (or delete) #{__FILE__}"
-#   end
-# end
+RSpec.describe "Posts", type: :request do
+  describe "GET /index" do
+    before(:example) { get('/users/745/posts') }
+    it 'check status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders correct template' do
+      expect(response).to render_template('index')
+    end
+
+    it 'check for placeholder text' do
+      expect(response.body).to include 'Posts of user'
+    end
+  end
+
+  describe "GET /index" do
+    before(:example) { get('/users/745/posts/4') }
+    it 'check status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders correct template' do
+      expect(response).to render_template('show')
+    end
+
+    it 'check for placeholder text' do
+      expect(response.body).to include 'Posts 4 by user'
+    end
+  end
+end
