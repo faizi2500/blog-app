a9542d3bcb48cd72a82bb4b28ca438826e4d2a3e
faizi2500 <faizan.zahid2500@gmail.com>
2021-12-08 22:56:51 +0500

    Update methods for index and show to render the right views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:id])
+    calc_posts(@user)
+  end
 
-  def show; end
+  def show
+    @urlarray = request.path.split('/')
+    @user = User.find(@urlarray[2])
+    @post = Post.find(params[:id])
+  end
 end
