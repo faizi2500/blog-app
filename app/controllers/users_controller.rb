20de72e1e85841d05e6aca979d73541b232ce92b
faizi2500 <faizan.zahid2500@gmail.com>
2021-12-08 18:53:23 +0500

    Extract data using models in the methods to display in view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,39 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+    @users.each do |user|
+    calc_posts(user)
+    end
+  end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+    @posts_by_user = calc_posts(@user)
+    @three_posts = []
+    @comment_count = Comment.group(:post_id).count
+    @posts_by_user.each_with_index do |val, i|
+      if i < 3
+        @three_posts.push(val)
+      end
+    end
+  end 
+
+  def calc_posts(user)
+    @posts_by_user = []
+    Post.all.order("created_at DESC").each do |post|
+      @posts_by_user.push(post) if post.user_id == user.id
+    end
+    user.post_counter = @posts_by_user.length
+    @posts_by_user
+  end
+
+  # def calc_comments(user, post)
+  #   @comment_list 
+  #   Comment.all.each do |comment|
+      
+  # comments = Comment.where("post_id = ?", val.id)
+  # likes = Like.where("post_id = ?", val.id)
+  # @counts.push([comments, likes])
+  #   end
+  # end
 end
