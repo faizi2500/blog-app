a07554f0f78a61149642eb2083ea318330b0b540
faizi2500 <faizan.zahid2500@gmail.com>
2021-12-08 22:56:15 +0500

    Move calc posts method to the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   def current_user
     User.first
   end
+
+  def calc_posts(user)
+    @posts_by_user = []
+    Post.all.order('created_at DESC').each do |post|
+      @posts_by_user.push(post) if post.user_id == user.id
+    end
+    user.post_counter = @posts_by_user.length
+    user.save
+    @posts_by_user
+  end
 end
