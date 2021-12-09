e2e3cc3a5fb186226a85bb5c95d488bdc2dfb574
faizi2500 <faizan.zahid2500@gmail.com>
2021-12-10 00:17:30 +0500

    Update routing to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#index'
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
-  get '/users/:id/posts', to: 'posts#index'
-  get '/users/:id/posts/:id', to: 'posts#show'
+  resources :users do
+    resources :posts 
+  end
+
+  resources :posts do
+    resources :comments, only: [:create]
+  end
 end
