9290d64fc01e3463a7459cb8ecc2e22963ca4627
faizi2500 <faizan.zahid2500@gmail.com>
2021-12-10 02:59:54 +0500

    Update routes for all controller and actions

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
 
   resources :posts do
-    resources :comments, only: [:create]
-    resources :likes, only: [:create]
+    resources :comments
+    resources :likes, only: [:new, :create]
   end
 end
