1dabe764a6c308100d86384ea305bb03be2ee42a
faizi2500 <faizan.zahid2500@gmail.com>
2021-12-09 12:50:12 +0500

    Add helper methods to get the comments and like counts and display all the comments

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def all_likes(id)
+    Like.where('post_id = ?', id).count
+  end
+
+  def all_comments(id)
+    Comment.where('post_id = ?', id).count
+  end
+
+  def list_comments(id)
+    Comment.where('post_id = ?', id).order('created_at DESC')
+  end
 end
