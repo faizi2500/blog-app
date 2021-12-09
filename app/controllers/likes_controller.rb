56fb5999210b9e40fdda86a02ff0a34a35ef6264
faizi2500 <faizan.zahid2500@gmail.com>
2021-12-10 00:19:31 +0500

    Create controller for likes so current_user can like the posts

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,5 @@
+class LikesController < ApplicationController
+  def create 
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   resources :posts do
     resources :comments, only: [:create]
+    resources :likes, only: [:create]
   end
 end
