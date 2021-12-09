f855ab683fbfd06d2f63aa191ddad8c0d3b4ae78
faizi2500 <faizan.zahid2500@gmail.com>
2021-12-10 02:57:50 +0500

    Add CRUD methods in the controllers to allow user to create new comments and like the existing posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,29 @@
 class CommentsController < ApplicationController
-  def create 
 
+  def index
+    @urlarray = request.path.split('/')
+    @post = Post.find(params[:post_id])
+  end
+
+  def new
+    @current = current_user
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+    render :new
+  end
+
+  def create 
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to post_comments_path
+      flash[:message] = 'Post was successfully created.'
+    else
+      render :new 
+      flash[:message] = 'Post not created. Please try again!'
+  end
+end
+
+  def comment_params
+    params.require(:comment).permit(:text, :user_id, :post_id)
   end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,30 @@
 class LikesController < ApplicationController
-  def create 
+  def new
+    @current = current_user
+    @post = Post.find(params[:post_id])
+    @like = Like.new
+  end
 
+  def create 
+    @like = Like.new(like_params)
+    @like.user_id = current_user.id
+    @allLikes = Like.all 
+    @status = true
+    @allLikes.each do |each|
+      if @like.user_id == each.user_id && @like.post_id == each.post_id
+        @status = false
+      end
+    end
+    if @status
+      @like.save
+      redirect_to posts_path
+      flash[:message] = 'Post was successfully created.'
+    else
+      redirect_to posts_path
+    end
+  end
+
+  def like_params
+    params.require(:like).permit(:post_id)
   end
 end
