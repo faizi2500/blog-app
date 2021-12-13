9ad0b1a1a9f16c2fe04717b7f976d7983210d876
faizi2500 <faizan.zahid2500@gmail.com>
2021-12-13 23:53:24 +0500

    Set up devise gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   add_flash_types :error, :success
-  def current_user
-    User.last
+
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
   end
 
   def calc_posts(user)
