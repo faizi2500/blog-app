6d37f708bbd4388906e4fad7db779417aecfe6c4
ridaarif98 <ridaarif16@gmail.com>
2021-12-14 17:26:50 +0100

    Fix required changes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,11 +10,11 @@ class Ability
     else
       can :read, :all
       can :destroy, Comment do |comment|
-        comment.user.id = user.id
+        comment.user.id == user.id
       end
 
       can :destroy, Post do |post|
-        post.user.id = user.id
+        post.user.id == user.id
       end
     end
     #
