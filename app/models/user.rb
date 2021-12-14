eced46598b28f3bad5ff559c1a3859e3c226d38d
ridaarif98 <ridaarif16@gmail.com>
2021-12-14 13:29:04 +0100

    Add roles in ability and user model class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+
+  after_create :set_roles
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -11,5 +13,11 @@ class User < ApplicationRecord
 
   def recent_posts
     posts.limit(3).order(created_at: :desc)
+  end
+
+  private 
+
+  def set_roles
+    self.update(role: 'user')
   end
 end
