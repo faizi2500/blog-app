62584232150e9c1397a39613461bf62088b321a3
faizi2500 <faizan.zahid2500@gmail.com>
2021-12-15 17:44:52 +0500

    Fix rubocop errors

--- spec/features/login/login_spec.rb
+++ spec/features/login/login_spec.rb
@@ -1,32 +1,32 @@
 require 'rails_helper'
-RSpec.feature "Login", type: :feature do
+RSpec.feature 'Login', type: :feature do
   it 'can enter a name and receive a greeting' do
     visit user_session_path
-    expect(page).to have_content "Log in"
+    expect(page).to have_content 'Log in'
   end
 
   it 'user can see inputs and button' do
     visit user_session_path
-    expect(page).to have_current_path(user_session_path)  
+    expect(page).to have_current_path(user_session_path)
   end
 
   it 'click the login error' do
     visit user_session_path
     click_button 'Log in'
-    expect(page).to have_content "Invalid Email or password."
+    expect(page).to have_content 'Invalid Email or password.'
   end
 
   it 'click the login error' do
-    user = User.create(name: 'Testing', post_counter: 0, email: 'user@example.com', password: 'password')
+    User.create(name: 'Testing', post_counter: 0, email: 'user@example.com', password: 'password')
     visit user_session_path
     fill_in 'email', with: 'user@example.com'
     fill_in 'pwd', with: 'password'
     click_button 'Log in'
-    expect(page).to have_content "Signed in successfully."
+    expect(page).to have_content 'Signed in successfully.'
   end
 
   it 'Login successfully' do
-    user = User.create(name: 'Testing', post_counter: 0, email: 'user@example.com', password: 'password')
+    User.create(name: 'Testing', post_counter: 0, email: 'user@example.com', password: 'password')
     visit user_session_path
     fill_in 'email', with: 'user@example.com'
     fill_in 'pwd', with: 'password'
@@ -35,11 +35,10 @@ RSpec.feature "Login", type: :feature do
   end
 
   it 'click the login error' do
-    user = User.create(name: 'Testing', post_counter: 0, email: 'user@example.com', password: 'password')
+    User.create(name: 'Testing', post_counter: 0, email: 'user@example.com', password: 'password')
     visit user_session_path
     fill_in 'email', with: 'user@example.com'
     click_button 'Log in'
-    expect(page).to have_content "Invalid Email or password."
+    expect(page).to have_content 'Invalid Email or password.'
   end
-
 end
--- spec/features/users/user_index_spec.rb
+++ spec/features/users/user_index_spec.rb
@@ -1,39 +1,36 @@
 require 'rails_helper'
-RSpec.feature "User Index", type: :feature do
-   
-    before (:each) do
-        @first_user =  User.create(name: 'Rous' ,email: 'other@example.com', password: 'rous12', post_counter: 0)
-        @second_user =  User.create(name: 'Ali' ,email: 'ali@example.com', password: '123456', post_counter: 1)
-        @third_user =  User.create(name: 'Ahmed' ,email: 'ahmed@example.com', password: '1234ahmed', post_counter: 3)
-        visit user_session_path
-        fill_in 'email', with: 'other@example.com'
-        fill_in 'pwd', with: 'rous12'
-        click_button 'Log in'
-    end
-    
-    it 'first user logged in' do
-        visit root_path
-        expect(page).to have_content "Rous"
-        expect(page).to have_content "Ali"
-        expect(page).to have_content "Ahmed"
-    end
+RSpec.feature 'User Index', type: :feature do
+  before(:each) do
+    @first_user = User.create(name: 'Rous', email: 'other@example.com', password: 'rous12', post_counter: 0)
+    @second_user = User.create(name: 'Ali', email: 'ali@example.com', password: '123456', post_counter: 1)
+    @third_user = User.create(name: 'Ahmed', email: 'ahmed@example.com', password: '1234ahmed', post_counter: 3)
+    visit user_session_path
+    fill_in 'email', with: 'other@example.com'
+    fill_in 'pwd', with: 'rous12'
+    click_button 'Log in'
+  end
 
-    it 'display third user have 3 posts' do
-        visit root_path
-        expect(page).to have_content "Number of posts 3"
-        expect(page).to have_content "Number of posts 0"
-        expect(page).to have_content "Number of posts 1"
-    end
+  it 'first user logged in' do
+    visit root_path
+    expect(page).to have_content 'Rous'
+    expect(page).to have_content 'Ali'
+    expect(page).to have_content 'Ahmed'
+  end
 
-    it 'page has user image tag' do
-        visit root_path
-        expect(page).to have_selector( 'img[alt="user_image"]' )
-        
-    end
+  it 'display third user have 3 posts' do
+    visit root_path
+    expect(page).to have_content 'Number of posts 3'
+    expect(page).to have_content 'Number of posts 0'
+    expect(page).to have_content 'Number of posts 1'
+  end
 
-    it 'display third user have 3 posts' do
-        click_on('Rous')
-        expect(page).to have_current_path(user_path(@first_user.id))
-    end
+  it 'page has user image tag' do
+    visit root_path
+    expect(page).to have_selector('img[alt="user_image"]')
+  end
 
+  it 'display third user have 3 posts' do
+    click_on('Rous')
+    expect(page).to have_current_path(user_path(@first_user.id))
+  end
 end
